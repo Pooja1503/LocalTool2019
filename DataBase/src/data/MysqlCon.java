8fc1f502de1126b2c56a170a3387a223914724b6
Ravi <Ravi@Pooja-PC>
2019-11-20 18:28:09 +0530

    adding new output

--- DataBase/src/data/MysqlCon.java
+++ DataBase/src/data/MysqlCon.java
@@ -17,7 +17,7 @@ public class MysqlCon {
 	ResultSet rs=stmt.executeQuery("select * from products");
 
 	while(rs.next())
-	System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+	System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3)+" "+rs.getString(4));
 
 	con.close();
 
